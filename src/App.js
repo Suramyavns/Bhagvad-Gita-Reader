afa8a5ad1e37564204ca8da646f155d41448a46c
leonardo <leonardo@kali.kali>
2024-03-31 08:35:34 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import logo from './bglogo.png';
 import './App.css';
+import music from './bgmusic.mp3'
+import { useState } from 'react';
+import 'font-awesome/css/font-awesome.min.css'
 
 function App() {
+  const [state,setState] = useState({
+    'play':false,
+    'btntext':'Play OM chant',
+  });
+  const [audio] = useState(new Audio(music));
+  audio.loop=true;
+  const handleAudioPlay = ()=>{
+    if(!state.play){
+      audio.play();
+      setState({'play':true,'btntext':'Pause OM chant'})
+    }
+    else{
+      audio.pause();
+      setState({'play':false,'btntext':'Play OM chant'})
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header className='App-header'>
+      <img src={logo} className='logo' alt='Bhagvad Gita Logo'></img>
+      <p id='top' className='heading'>Read Bhagvad Geeta</p>
+      <div className='nav-btns'>
+        <button className='nav-btn'>Full Read</button>
+        <button className='nav-btn'>Chapters</button>
+      </div>
+      <button className='nav-btn' style={{backgroundColor:'#FF784F',width:'fit-content'}} onClick={handleAudioPlay}>{state.btntext}</button>
+    </header>
+    <main className='main-container'></main>
+    <footer className='App-header'>
+      <p className='heading'>Made with <i className='fa fa-heart' style={{color:'maroon'}}></i>  by Suramya Didwania</p>
+    </footer>
+    </>
   );
 }
 
