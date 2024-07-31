36498d82b464c340137a4371ffc5993d2bd7703c
suramyavns <sdidwania645@gmail.com>
2024-07-31 12:29:25 +0530

    api

---
+++ src/rapidapi.js
@@ -0,0 +1,9 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c60c2a06e1mshb4f017f597a0211p18557ajsn2fad4e30d3ca",
+    "X-RapidAPI-Host": "bhagavad-gita3.p.rapidapi.com",
+  },
+};
+
+export default options;
